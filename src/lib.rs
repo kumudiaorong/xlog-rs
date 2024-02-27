094a5e60e83f0475dfc5139021dd917301a95aed
kumudiaorong <kmdr.error@gmail.com>
2024-02-27 16:39:20 +0800

    feat:add example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 //! xlog-rs
 //!
 //! A simple logging library for Rust.
-
+//!
+//! # Examples
+//!
+//! ```
+//! use xlog_rs::warn;
+//!
+//! xlog_rs::log::init(std::io::stdout(), xlog_rs::log::Level::Trace);
+//!
+//! warn!("warn message");
+//! ```
 /// main module
 pub mod log;
